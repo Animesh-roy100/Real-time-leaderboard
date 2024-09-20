7447b6e88cd83d6de5eefb575e267941a9e7873d
Animesh-roy100 <royanimesh7478@gmail.com>
2024-09-20 10:15:54 +0530

    add player in redis stream added

--- leaderboard-backend/producer/index.js
+++ leaderboard-backend/producer/index.js
@@ -1,14 +1,39 @@
 const express = require("express");
+const Redis = require("ioredis");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+const client = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
 
 app.use(express.json());
 
-app.post("/processData", (req, res) => {
-    console.log(req.body);
-    res.send("Data received");
+const streamName = "leaderboard";
+
+app.post("/addPlayer", async (req, res) => {
+  const data = req.body;
+  console.log("player info: ", req.body);
+  try {
+    await client.xadd(streamName, "*", "players", JSON.stringify(data));
+    res.status(200).json({ message: "Player added successfully" });
+  } catch (error) {
+    console.error("Error adding player:", error);
+    res
+      .status(500)
+      .json({ error: "Error adding player", details: error.message });
+  }
 });
 
 app.listen(4000, () => {
-    console.log('Producer running on port 4000');
-})
+  console.log("Producer running on port 4000");
+});
---
+++ leaderboard-frontend/src/app/action.tsx
@@ -0,0 +1,19 @@
+export type PlayerProps = {
+  playerId: number;
+  playerName: string;
+  score: number;
+  createdOn: string;
+};
+export default async function addPlayerAction(playerInfo: PlayerProps) {
+  console.log("playerInfo", playerInfo);
+
+  const response = await fetch("http://localhost:4000/addPlayer", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(playerInfo),
+  });
+
+  return response;
+}
